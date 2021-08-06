0709c48bf463ca1b33c7262a8856e912467330a7
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-08-06 11:15:52 -0700

    update migration

---
+++ Factory/Migrations/20210806180138_Third.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Third : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "Machines",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Machines",
+                newName: "DateCreated");
+        }
+    }
+}
