2faefafbfa0c096ccaf303f9581439babd79c25f
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-08-08 16:25:38 -0700

    add logic to display both lists of engineers/machines on splash page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+    private readonly FactoryContext _db;
 
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List<Engineer> engineerList = _db.Engineers.ToList();
+      List<Machine> machineList = _db.Machines.ToList();
+      ViewBag.Engineers = engineerList;
+      ViewBag.Machines = machineList;
+      return View();
+    }
+      
 
     }
 }
